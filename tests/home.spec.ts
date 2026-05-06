7e644b74b6c54c600ce3bfa77d796f5c2e18e8fd
deependra8888 <deepender8888@gmail.com>
2026-05-07 00:58:05 +0530

    Initial project setup

---
+++ tests/home.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test("homepage loads", async ({ page }) => {
+  await page.goto("http://localhost:5173");
+
+  await expect(
+    page.locator("h1")
+  ).toContainText("Products");
+});
